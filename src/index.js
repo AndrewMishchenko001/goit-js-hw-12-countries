cd244c311045d8b5cbea99c447cfa600d191a249
AndrewMishchenko <AndrewMishchenko001@gmail.com>
2021-08-25 00:25:54 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+import './js/fetchContries';
+
+
+
+// const f = fetch('https://restcountries.eu/rest/v2/name/ukraine').then(response => {
+  
+//     return response.json()
+// })
+//     .then(capital => { console.log(...capital) })
+//     .catch(error => { console.log(error);});
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+
+    input: document.querySelector('.js-input'),
+    cardContainer: document.querySelector('.js-card-container'),
+    form: document.querySelector(".form"),
+}
